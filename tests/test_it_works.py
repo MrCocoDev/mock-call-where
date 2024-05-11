4c5ff7298a63982fd6bdad3d6d84ebccf8b98924
Collin Sage <3229549+MrSage@users.noreply.github.com>
2024-05-11 18:00:34 -0300

    chore: Add a test to make sure the patch is only selectively applied.

--- tests/test_it_works.py
+++ tests/test_it_works.py
@@ -8,9 +8,28 @@ def test_mock_call_has_a_where(mock_where):
     my_mock.a_function()
     the_call = my_mock.mock_calls[0]
 
-    assert hasattr(the_call, "where")
     assert type(the_call) is AdvancedCall
     assert (
         "in test_mock_call_has_a_where\n    my_mock.a_function()\n"
         in the_call.where[-4]
     )
+
+
+def test_advanced_call_is_not_automatically_applied():
+    my_mock = mock.Mock()
+
+    my_mock.a_function()
+    call1 = my_mock.mock_calls[0]
+    assert type(call1) is mock._Call
+
+    AdvancedCall.apply()
+
+    my_mock.another_function()
+    call2 = my_mock.mock_calls[1]
+    assert type(call2) is AdvancedCall
+
+    AdvancedCall.unapply()
+
+    my_mock.a_third_function()
+    call3 = my_mock.mock_calls[2]
+    assert type(call3) is mock._Call
